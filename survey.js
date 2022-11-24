84eaa661397936f0a0ab65e61b7254b704b8bc89
Manzar <manzar.hussain@telus.com>
2022-11-23 19:04:06 -0500

    Fun Profile Generator

--- survey.js
+++ survey.js
@@ -3,10 +3,7 @@ const readline = require('readline');
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
-});
-
-
-  
+});  
   
 rl.question('What\'s your name? Nicknames are also acceptable: ', (answer) => {
   const name = answer;
